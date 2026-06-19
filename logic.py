aafcdc74d6f5a77394f727d36207115cda44e42e
Yin Yang <mihiparis@gmail.com>
2026-06-19 15:02:02 +0300

    part 1 section 1-2

---
+++ logic.py
@@ -0,0 +1,6 @@
+# part 1 section 1
+is_online = True
+has_access = False
+print (f'is online and has access : {is_online and has_access}' )
+# part 1 section 2
+print (f'is online or has access : {is_online or has_access}' )
