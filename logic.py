93dddad56588b37e89ebbee4f730da62d59a05c3
Yin Yang <mihiparis@gmail.com>
2026-06-19 15:03:16 +0300

    part 1 section 3-5

--- logic.py
+++ logic.py
@@ -4,3 +4,13 @@ has_access = False
 print (f'is online and has access : {is_online and has_access}' )
 # part 1 section 2
 print (f'is online or has access : {is_online or has_access}' )
+# part 1 section 3
+status = False
+print(f' status is not  : {not status}')
+# part 1 section 4
+age = 20
+has_id = True
+print(f' the person can enter {age > 18 and has_id == True}')
+# part 1 section 5
+level = 3
+print (f' level is {1 <= level <= 5}')
