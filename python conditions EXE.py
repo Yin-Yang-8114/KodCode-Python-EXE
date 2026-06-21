559f578e6c26a8b1c3366e0b94058d49a7149ecb
Yin Yang <mihiparis@gmail.com>
2026-06-21 13:19:50 +0300

    uploaded sections 1-4

---
+++ python conditions EXE.py
@@ -0,0 +1,28 @@
+# part 1 section 1
+age = int(input("Enter your age : "))
+if age >= 18 :
+    print('can enter')
+elif age < 18 :
+    print('canot enter')
+# part 1 section 2
+temperature = 38.2
+if temperature >= 37.5 :
+    print('High temperature')
+else:
+    print('Normal temperature')
+# part 1 section 3
+number = int(input("Enter a number : "))
+if number % 2 == 0 :
+    print('even number')
+elif number % 2 != 0 :
+    print('odd number')
+# part 1 section 4
+battery = 15
+is_charging = True
+if battery > 20 and is_charging :
+    print('charging now')
+elif battery < 20 and is_charging ==False:
+    print('Low battery')
+else :
+    print('Battery OK')
+# part 1 section 5
