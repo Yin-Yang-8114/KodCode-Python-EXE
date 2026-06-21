c7e5b50d829727ce78a46c18f99f4fadc6b2bdd7
Yin Yang <mihiparis@gmail.com>
2026-06-21 15:15:12 +0300

    added section 2

--- python condisions EX advanced.py
+++ python condisions EX advanced.py
@@ -1,3 +1,4 @@
+# section 1
 menu_choice = input(f'Please enter your choice: \n 1) forest \n 2) cave \n 3) river\n')
 if menu_choice == "forest":
     forest_choice = str(input('choose tp hide or walk : '))
@@ -23,4 +24,16 @@ elif menu_choice == "river":
     print('You find a boat')
 else:
     print('unknown place')
-
+# section 2
+num1 = float(input('enter your first number: : '))
+num2 = float(input('enter your second number: : '))
+calculator_action = str(input(f'choose your calculator action : \n 1) add\n 2) subtract\n 3) multiply\n'))
+if calculator_action == "add" or "1" :
+    print('the add of the tow numbers is : ',num1 + num2)
+elif calculator_action == "subtract" or "2" :
+    print('the subtract of the tow numbers is : ',num1 - num2)
+elif calculator_action == "multiply" or "3" :
+    print('the multiply of the tow numbers is : ',num1 * num2)
+else:
+    print('Invalid calculator action')
+# section 3
