d36089a27769cad6d17a819ab6d1234e92fa08ee
Yin Yang <mihiparis@gmail.com>
2026-06-21 16:08:05 +0300

    added section 5

--- python condisions EX advanced.py
+++ python condisions EX advanced.py
@@ -48,20 +48,34 @@
 # else :
 #     print('INVALID CHOICE')
 # section 4
-correct_pin = 4321
-balance = 500
-user_PIN_input = int(input('Enter your PIN: '))
-if user_PIN_input == correct_pin:
-    Withdrawal=int(input('how much you want to withdraw?: '))
-    if Withdrawal>balance:
-        print('not enough money')
-    else:
-        want_receipt = input('do yo want to receipt?: yes/no ')
-        if want_receipt=='yes':
-            print('Withdrawal approved with receipt')
-        elif want_receipt=='no':
-            print('Withdrawal approved without receipt')
-        else:
-            print('Withdrawal approved')
+# correct_pin = 4321
+# balance = 500
+# user_PIN_input = int(input('Enter your PIN: '))
+# if user_PIN_input == correct_pin:
+#     Withdrawal=int(input('how much you want to withdraw?: '))
+#     if Withdrawal>balance:
+#         print('not enough money')
+#     else:
+#         want_receipt = input('do yo want to receipt?: yes/no ')
+#         if want_receipt=='yes':
+#             print('Withdrawal approved with receipt')
+#         elif want_receipt=='no':
+#             print('Withdrawal approved without receipt')
+#         else:
+#             print('Withdrawal approved')
+# else:
+#     print('wrong password')
+# section 5
+order_price = float(input('enter your order price: '))
+if order_price <= 50:
+    print('order to small for delivery')
 else:
-    print('wrong password')
+    club_number = input('enter your if you have club number yes/no : ')
+    if club_number == 'yes':
+        coupon = input('do you have a coupon ?  yes / no : ')
+        if coupon == 'yes':
+            print('you got free delivery and 10% discount')
+        else:
+            print('you got only free delivery')
+    else:
+        print('delivery cost is 15$')
