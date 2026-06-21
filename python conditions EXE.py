97542c7bf4647f847ccced8af7b5fe1e5263eeb7
Yin Yang <mihiparis@gmail.com>
2026-06-21 13:55:52 +0300

    uploaded sections 7-10

--- python conditions EXE.py
+++ python conditions EXE.py
@@ -45,7 +45,32 @@ second_number = int(input("Enter the second number : "))
 if first_number > second_number :
     print('First is bigger')
 elif second_number == first_number :
-    print('equal')
+    print(' the first number and the second number are equal')
 else:
     print('second is bigger')
 # part 1 section 8
+fuel = 40
+distance = 30
+reserve = fuel - distance
+if fuel - distance >= 10 :
+    print('enough fuel with reserve')
+elif fuel - distance >= 0 and reserve < 10 :
+    print('enough fuel , low reserve')
+else:
+    print('not enough fuel')
+# part 1 section 9
+username = str(input("Enter your username : "))
+if username == '' :
+    print('guest user')
+else :
+    print('Hello', username)
+# part 1 section 10
+hour = 21
+if hour < 0 or hour > 23 :
+    print('invalid hour')
+elif hour > 12:
+    print('Good Morning')
+elif hour == 18:
+    print('Good Afternoon')
+else :
+    print('Good Evening')
