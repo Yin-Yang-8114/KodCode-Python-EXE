ebe657af039adf8c90f5ad66569f41da14538437
Yin Yang <mihiparis@gmail.com>
2026-06-21 15:45:56 +0300

    added section 3

--- python condisions EX advanced.py
+++ python condisions EX advanced.py
@@ -1,39 +1,49 @@
 # section 1
-menu_choice = input(f'Please enter your choice: \n 1) forest \n 2) cave \n 3) river\n')
-if menu_choice == "forest":
-    forest_choice = str(input('choose tp hide or walk : '))
-    if forest_choice == "hide":
-       print('You hide behind a tree ')
-    elif forest_choice == "walk":
-        print('You find a sleeping wolf')
-    else:
-       print('Invalid forest action')
-elif menu_choice == "cave":
-    cave_choice = str(input('do you have a torch : '))
-    if cave_choice == "yes":
-        cave_dircetions = str(input('you want to go left or to the right  : '))
-        if cave_dircetions == "left":
-            print('You find gold  ')
-        elif cave_dircetions == "right":
-            print('You find bats')
-        else:
-            print('Invalid path')
-    else :
-        print('Its too dark to enter')
-elif menu_choice == "river":
-    print('You find a boat')
-else:
-    print('unknown place')
+# menu_choice = input(f'Please enter your choice: \n 1) forest \n 2) cave \n 3) river\n')
+# if menu_choice == "forest":
+#     forest_choice = str(input('choose tp hide or walk : '))
+#     if forest_choice == "hide":
+#        print('You hide behind a tree ')
+#     elif forest_choice == "walk":
+#         print('You find a sleeping wolf')
+#     else:
+#        print('Invalid forest action')
+# elif menu_choice == "cave":
+#     cave_choice = str(input('do you have a torch : '))
+#     if cave_choice == "yes":
+#         cave_dircetions = str(input('you want to go left or to the right  : '))
+#         if cave_dircetions == "left":
+#             print('You find gold  ')
+#         elif cave_dircetions == "right":
+#             print('You find bats')
+#         else:
+#             print('Invalid path')
+#     else :
+#         print('Its too dark to enter')
+# elif menu_choice == "river":
+#     print('You find a boat')
+# else:
+#     print('unknown place')
 # section 2
-num1 = float(input('enter your first number: : '))
-num2 = float(input('enter your second number: : '))
-calculator_action = str(input(f'choose your calculator action : \n 1) add\n 2) subtract\n 3) multiply\n'))
-if calculator_action == "add" or "1" :
-    print('the add of the tow numbers is : ',num1 + num2)
-elif calculator_action == "subtract" or "2" :
-    print('the subtract of the tow numbers is : ',num1 - num2)
-elif calculator_action == "multiply" or "3" :
-    print('the multiply of the tow numbers is : ',num1 * num2)
-else:
-    print('Invalid calculator action')
+# num1 = float(input('enter your first number: : '))
+# num2 = float(input('enter your second number: : '))
+# calculator_action = input(f'choose your calculator action : \n 1) add\n 2) subtract\n 3) multiply\n')
+# if calculator_action == "add" or calculator_action == "1" :
+#     print('the add of the tow numbers is : ',num1 + num2)
+# elif calculator_action == "subtract" or calculator_action == "2" :
+#     print('the subtract of the tow numbers is : ',num1 - num2)
+# elif calculator_action == "multiply" or calculator_action == "3"  :
+#     print('the multiply of the tow numbers is : ',num1 * num2)
+# else:
+#     print('Invalid calculator action')
 # section 3
+# computer_choice = "rock"
+# user_chose_for_rock_etc = str(input(f'choose \n 1) rock \n 2) paper \n 3) scissors\n'))
+# if user_chose_for_rock_etc == computer_choice:
+#     print('drow')
+# elif user_chose_for_rock_etc == "paper":
+#     print('you win')
+# elif user_chose_for_rock_etc == "scissors":
+#     print('you lose')
+# else :
+#     print('INVALID CHOICE')
