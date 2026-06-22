244065fe7195eea3e6ec21f74c2d697f249efeba
Yin Yang <mihiparis@gmail.com>
2026-06-22 14:56:02 +0300

    added part 2

--- roster.py
+++ roster.py
@@ -26,3 +26,32 @@ print(min(scores))
 agents2 = agents.copy()
 agents2[0]="ZuZU"
 print(f'agents : {agents}\nagents 2 : {agents2}')
+# part 2 section 1
+numbers = [3, 1, 4, 1, 5, 9, 2, 6]
+numbers.sort()
+print(numbers)
+numbers = [3, 1, 4, 1, 5, 9, 2, 6]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers, numbers)
+# part 2 section 2
+a = [1, 2, 3]
+b = [4, 5, 6]
+c= a + b
+print(c)
+a.extend(b)
+print(a)
+# part 2 section 3
+items = ['x', 'y', 'z', 'x', 'y', 'x']
+print(items.count('x'))
+items.remove('x')
+print(items)
+items.pop()
+print(items)
+del items[2]
+print(items)
+# part 2 section 4
+data = [1, 2, 3, 4, 5]
+print(data[0::2])
+# part 2 section 5
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(matrix[1][1])
